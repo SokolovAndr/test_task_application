4486dfc79489db319997ede20779e57be9a75de0
SokolovAndr <korch45@yandex.ru>
2025-12-15 09:16:00 +0300

    Добавил Drawer для навигации

--- lib/features/products/presentation/screens/products_screen.dart
+++ lib/features/products/presentation/screens/products_screen.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:test_task_application/core/presentation/widgets/app_drawer.dart';
 
 @RoutePage()
 class ProductsScreen extends StatelessWidget {
@@ -9,6 +10,7 @@ class ProductsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('ProductsScreen')),
+      drawer: AppDrawer(),
       body: Center(child: Text('Это ProductsScreen')),
     );
   }
--- lib/features/profile/presentation/screens/profile_screen.dart
+++ lib/features/profile/presentation/screens/profile_screen.dart
@@ -1,5 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:test_task_application/core/presentation/widgets/app_drawer.dart';
+import 'package:test_task_application/generated/l10n.dart';
 
 @RoutePage()
 class ProfileScreen extends StatelessWidget {
@@ -8,7 +10,8 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('ProfileScreen')),
+      appBar: AppBar(title: Text(S.current.profile)),
+      drawer: AppDrawer(),
       body: Center(child: Text('Это ProfileScreen')),
     );
   }
