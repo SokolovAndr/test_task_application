670610a7d93bb5e66b5f529c1498f2677b149264
SokolovAndr <korch45@yandex.ru>
2025-12-16 12:19:27 +0300

    работа с users->presentaion1

--- lib/features/users/presentation/widgets/users_list_item_titled_component_widget.dart
+++ lib/features/users/presentation/widgets/users_list_item_titled_component_widget.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:test_task_application/core/utils/themes/app_colors.dart';
 import 'package:test_task_application/core/utils/themes/font_family.dart';
@@ -16,35 +15,28 @@ class UsersListItemTitledComponentWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return Row(
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                title,
-                style: TextStyle(
-                  fontSize: 14,
-                  color: theme.extension<AppColors>()?.base500,
-                  fontWeight: FontWeight.w400,
-                  height: 1,
-                ).def(),
-              ),
-              const SizedBox(height: 6),
-              Container(
-                padding: const EdgeInsets.only(right: 16),
-                child: Text(
-                  label ?? '',
-                  style: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400,
-                    height: 1.5,
-                  ).def(),
-                ),
-              ),
-            ],
+        Text(
+          title,
+          style: TextStyle(
+            fontSize: 14,
+            color: theme.extension<AppColors>()?.base500,
+            fontWeight: FontWeight.w400,
+            height: 1,
+          ).def(),
+        ),
+        const SizedBox(height: 6),
+        Container(
+          padding: const EdgeInsets.only(right: 16),
+          child: Text(
+            label ?? '',
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w400,
+              height: 1.5,
+            ).def(),
           ),
         ),
       ],
--- lib/features/users/presentation/widgets/users_list_item_widget.dart
+++ lib/features/users/presentation/widgets/users_list_item_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:test_task_application/core/utils/themes/app_colors.dart';
 import 'package:test_task_application/features/users/domain/entities/user_entity.dart';
 import 'package:test_task_application/features/users/presentation/widgets/users_list_item_titled_component_widget.dart';
 import 'package:test_task_application/generated/l10n.dart';
@@ -10,23 +11,39 @@ class UsersListItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return ListTile(
+      leading: CircleAvatar(
+        radius: 30,
+        backgroundColor: theme.extension<AppColors>()!.primary40,
+        child: Icon(
+          Icons.person_2_outlined,
+          color: theme.extension<AppColors>()!.baseGreen,
+          size: 28,
+        ),
+      ),
       contentPadding: EdgeInsets.all(8),
-      title: Column(
+      tileColor: theme.extension<AppColors>()!.baseWhite,
+      title: Row(
         children: [
-          UsersListItemTitledComponentWidget(
-            title: S.current.firstname,
-            label: user.name.firstname,
-          ),
-          SizedBox(height: 8),
-          UsersListItemTitledComponentWidget(
-            title: S.current.lastname,
-            label: user.name.lastname,
-          ),
-          SizedBox(height: 8),
-          UsersListItemTitledComponentWidget(
-            title: S.current.phone,
-            label: user.phone,
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              UsersListItemTitledComponentWidget(
+                title: S.current.firstname,
+                label: user.name.firstname,
+              ),
+              SizedBox(height: 8),
+              UsersListItemTitledComponentWidget(
+                title: S.current.lastname,
+                label: user.name.lastname,
+              ),
+              SizedBox(height: 8),
+              UsersListItemTitledComponentWidget(
+                title: S.current.phone,
+                label: user.phone,
+              ),
+            ],
           ),
         ],
       ),
