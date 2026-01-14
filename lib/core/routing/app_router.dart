55196081e0de670ef23314f2fbef26d78fc9b765
SokolovAndr <korch45@yandex.ru>
2026-01-14 20:45:48 +0300

    фикс роутинга2

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,30 +1,3 @@
-/*import 'package:auto_route/auto_route.dart';
-import 'package:flutter/material.dart';
-import 'package:test_task_application/features/auth/presentation/screens/auth_screen.dart';
-import 'package:test_task_application/features/carts/presentation/screens/cart_item_screen.dart';
-import 'package:test_task_application/features/carts/presentation/screens/carts_screen.dart';
-import 'package:test_task_application/features/products/presentation/screens/product_item_screen.dart';
-import 'package:test_task_application/features/products/presentation/screens/products_screen.dart';
-import 'package:test_task_application/features/profile/presentation/screens/profile_screen.dart';
-import 'package:test_task_application/features/users/presentation/screens/users_screen.dart';
-
-part 'app_router.gr.dart';
-
-@AutoRouterConfig()
-class AppRouter extends RootStackRouter {
-  @override
-  List<AutoRoute> get routes => [
-    AutoRoute(path: '/', page: AuthRoute.page, initial: true),
-    AutoRoute(path: '/profile/:id', page: ProfileRoute.page),
-    AutoRoute(path: '/products', page: ProductsRoute.page),
-    AutoRoute(path: '/:productId', page: ProductItemRoute.page),
-    AutoRoute(path: '/users/:id', page: UsersRoute.page),
-    AutoRoute(path: '/carts/:id', page: CartsRoute.page),
-    AutoRoute(path: '/cartItem', page: CartItemRoute.page),
-  ];
-}
-*/
-
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:test_task_application/features/auth/presentation/screens/auth_screen.dart';
